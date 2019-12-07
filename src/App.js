e4839fce49aa30999f1853c2cfb005868ebfb58f
Swaleh Mhina <cyph09@gmail.com>
2019-12-07 23:50:04 +0300

    Calculator structure and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div id="display">
+        <div className="formula">21+2334 x 0.3434 / 12</div>
+        <div className="answer">12323.3253</div>
+      </div>
+      <div className="buttons">
+        <div id="clear" className="btn">
+          AC
+        </div>
+        <div id="resert" className="btn">
+          C
+        </div>
+        <div id="back" className="btn">
+          Back
+        </div>
+        <div id="divide" className="btn">
+          /
+        </div>
+        <div id="seven" className="btn">
+          7
+        </div>
+        <div id="eight" className="btn">
+          8
+        </div>
+        <div id="nine" className="btn">
+          9
+        </div>
+        <div id="multiply" className="btn">
+          x
+        </div>
+        <div id="four" className="btn">
+          4
+        </div>
+        <div id="five" className="btn">
+          5
+        </div>
+        <div id="six" className="btn">
+          6
+        </div>
+        <div id="subtract" className="btn">
+          -
+        </div>
+        <div id="one" className="btn">
+          1
+        </div>
+        <div id="two" className="btn">
+          2
+        </div>
+        <div id="three" className="btn">
+          3
+        </div>
+        <div id="add" className="btn">
+          +
+        </div>
+        <div id="sign" className="btn">
+          +/-
+        </div>
+        <div id="zero" className="btn">
+          0
+        </div>
+        <div id="decimal" className="btn">
+          .
+        </div>
+        <div id="equals" className="btn">
+          =
+        </div>
+      </div>
     </div>
   );
 }
